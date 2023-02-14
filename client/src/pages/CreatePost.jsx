9cb0d03896788f4d3832d076b35be05fb1c17a4b
Nepail <kls18z@hotmail.com>
2023-02-14 15:26:35 +0800

    Client get dalle route

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,8 +16,28 @@ const CreatePost = () => {
   const [generatingImg, setgeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const generateImage = () => {
-    
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setgeneratingImg(true);
+        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        });
+
+        const data = await response.json();
+        setform({ ...form, photo: `data:image/jpeg;base64, ${data.photo}` });
+      } catch (error) {
+        alert(error);
+      } finally {
+        setgeneratingImg(false);
+      }
+    } else {
+      alert('Please enter a prompt')
+    }
   };
 
   const handleSubmit = () => {};
